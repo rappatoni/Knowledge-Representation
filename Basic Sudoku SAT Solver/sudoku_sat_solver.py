9e288b2e3b6292458bb18e569f0c02072ae01d83
rappatoni <maxgrapp@googlemail.com>
2017-09-28 23:05:57 +0200

    Make project outline compilable and add gitignore

--- Basic Sudoku SAT Solver/sudoku_sat_solver.py
+++ Basic Sudoku SAT Solver/sudoku_sat_solver.py
@@ -264,7 +264,7 @@ def logically_prune(learned_clauses):
 
     #Alternative (better) approach: record the sudoku solutions. Then for each clause  say (a or ~b)
     #check the solved sudokus. If there is a solved sudoku s.t. a is not in the solution and b is, then
-    # (a or ~b) is false in that sudoku and hence cannot be valid. 
+    # (a or ~b) is false in that sudoku and hence cannot be valid.
 
     #Check for supersets of encoding clauses (minimal and extended).
 
